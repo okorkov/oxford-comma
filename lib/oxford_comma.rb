00e4a0acbe54a11058c66ed2bc1beca10221888d
okorkov <alex.okorkov@gmail.com>
2020-11-10 11:48:04 -0800

    commit tree 5a603f246ad9e9ada4214371db61483d5fdfbd4b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    result = ""
+    if array.length == 1
+        return array[0]
+    end
+    if  array.length == 2
+        return "#{array[0]} and #{array[1]}"
+    end
+    if array.length > 2
+        array.each do |fruit|
+            if array.index(fruit) != array.index(array.last)
+                result << fruit + ', '
+            else
+                result << "and " + fruit
+            end
+        end
+    end
+    result
 end
